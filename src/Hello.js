a3bbb251aba1764de5d705e4fbaeadf11facb39e
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2023-01-23 11:32:45 +0530

    first changess

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hello = () => {
+  return (
+    <>
+    <h1>this is heading    </h1>
+                                  <div>Hello</div>
+      <p>this is a hello component.        </p>
+      
+
+      
+    </>
+  );
+};
+
+export default Hello;
